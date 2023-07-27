691bbfb28048921a609929c0787e095c15b219dd
CODE TITANS <139016736+C0DE-TITANS@users.noreply.github.com>
2023-07-27 06:38:42 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 #UTF-8
 from os import system as cmd
+from time import sleep as beep
 import platform
 bit = platform.architecture()[0]
+cmd('xdg-open https://chat.whatsapp.com/Fg47Y5iCKOc3Ma3q8IktUy')
 cmd('git pull');cmd('clear')
 
 if bit=='64bit':
-    print('[!] 64 Bit Device')
+    print('[!] 64 Bit Device');beep(2)
     cmd('chmod 777 titans')
     cmd('./titans')
 
 if bit == '32bit':
-    print('[!] 32 Bit Device')
+    print('[!] 32 Bit Device')beep(2)
     cmd('chmod 777 titan')
     cmd('./titan')
 
